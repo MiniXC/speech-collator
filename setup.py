57527e191fbce0892e9f18f43262ff96f8ed7564
Christoph Minixhofer <christoph.minixhofer@gmail.com>
2023-07-02 15:13:07 +0000

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="speech-collator",
-    version="0.1.11",
+    version="0.1.12",
     description="A collator for speech datasets with different batching strategies and attribute extraction.",
     author="Christoph Minixhofer",
     author_email="christoph.minixhofer@gmail.com",
